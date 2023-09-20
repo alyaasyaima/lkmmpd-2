f172fc4229e2360f4da0542804a40d07abbd8757
alyaasyaima <145540019+alyaasyaima@users.noreply.github.com>
2023-09-20 21:02:09 +0700

    Add files via upload

---
+++ LuasSegitiga.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author ACER
+ */
+public class LuasSegitiga {
+        public static void main(String[] args) {
+            double alas = 7;
+            double tinggi = 3; 
+            double luas = alas*tinggi / 2;
+            System.out.println("LuasSegitiga:"+ luas);
+
+}
+}
